442204513d799534f555ade2ccc0b8d79f15e659
Martin Benzo <martinbenzo@gmail.com>
2023-02-21 13:05:42 -0300

    Add existing project files to Git

---
+++ backend/server.js
@@ -0,0 +1,112 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  allowEIO3: true,
+  cors: {
+    origin: "http://127.0.0.1:8080",
+    //origin: "https://buenosairesrawfood.com.ar/snake/front",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const {
+  createGameState,
+  gameLoop,
+  getUpdatedVelocity,
+  initGame,
+} = require("./game");
+const { FRAME_RATE } = require("./constants");
+const { makeid } = require("./utils");
+
+const state = {};
+const clientRooms = {};
+
+io.on(`connection`, (client) => {
+  client.on(`keyDown`, handleKeyDown);
+  client.on(`newGame`, handleNewGame);
+  client.on(`joinGame`, handleJoinGame);
+
+  function handleNewGame() {
+    let roomName = makeid(5);
+    clientRooms[client.id] = roomName;
+    client.emit("gameCode", roomName);
+
+    state[roomName] = initGame();
+
+    client.join(roomName);
+    client.number = 1;
+    client.emit("init", 1);
+  }
+
+  function handleJoinGame(gameCode) {
+    // const room = io.sockets.adapter.rooms.get(gameCode);
+    // console.log("room", room);
+    // let allUsers;
+
+    // if (room) {
+    //   allUsers = room.sockets;
+    // }
+    // console.log("allUsers", allUsers);
+    // let numClients = 0;
+    // if (allUsers) {
+    //   numClients = Object.keys(allUsers).length;
+    // }
+    // console.log("numClients", numClients);
+    // if (numClients === 0) {
+    //   client.emit("unknownGame");
+    //   return;
+    // } else if (numClients > 1) {
+    //   client.emit("tooManyPlayers");
+    //   return;
+    // }
+
+    clientRooms[client.id] = gameCode;
+    client.join(gameCode);
+    client.number = 2;
+    client.emit("init", 2);
+    startGameInterval(gameCode);
+  }
+
+  function handleKeyDown(keyCode) {
+    const roomName = clientRooms[client.id];
+
+    if (!roomName) {
+      return;
+    }
+
+    try {
+      keyCode = parseInt(keyCode);
+    } catch (e) {
+      console.error(e);
+    }
+
+    const vel = getUpdatedVelocity(keyCode);
+
+    if (vel) {
+      state[roomName].players[client.number - 1].vel = vel;
+    }
+  }
+});
+
+function startGameInterval(roomName) {
+  const intervalId = setInterval(() => {
+    const winner = gameLoop(state[roomName]);
+    if (!winner) {
+      emitGameState(roomName, state[roomName]);
+    } else {
+      emitGameOver(roomName, winner);
+      state[roomName] = null;
+      clearInterval(intervalId);
+    }
+  }, 1000 / FRAME_RATE);
+}
+
+function emitGameState(roomName, state) {
+  io.sockets.in(roomName).emit("gameState", JSON.stringify(state));
+}
+
+function emitGameOver(roomName, winner) {
+  io.sockets.in(roomName).emit("gameOver", JSON.stringify({ winner }));
+}
+
+io.listen(3009);
---
+++ frontend/index.js
@@ -0,0 +1,130 @@
+const BG_COLOR = "#231f20";
+const SNAKE_COLOR = "#c2c2c2";
+const FOOD_COLOR = "#e66916";
+
+const socket = io("http://localhost:3009", {
+  //const socket = io("https://buenosairesrawfood.com.ar/snake/back", {
+  withCredentials: true,
+});
+
+socket.on("init", handleInit);
+socket.on("gameState", handleGameState);
+socket.on("gameOver", handleGameOver);
+socket.on("gameCode", handleGameCode);
+socket.on("unknownGame", handleUnknownGame);
+socket.on("tooManyPlayers", handleTooManyPlayers);
+
+const gameScreen = document.getElementById("gameScreen");
+const initialScreen = document.getElementById("initialScreen");
+const newGameBtn = document.getElementById("newGameButton");
+const gameCodeInput = document.getElementById("gameCodeInput");
+const joinGameButton = document.getElementById("joinGameButton");
+const gameCodeDisplay = document.getElementById("gameCodeDisplay");
+
+newGameBtn.addEventListener("click", newGame);
+joinGameButton.addEventListener("click", joinGame);
+
+function newGame() {
+  socket.emit("newGame");
+  init();
+}
+
+function joinGame() {
+  const gameCode = gameCodeInput.value;
+  socket.emit("joinGame", gameCode);
+  init();
+}
+
+let canvas, ctx;
+let playerNumber;
+let gameActive;
+
+function init() {
+  initialScreen.style.display = "none";
+  gameScreen.style.display = "block";
+
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+
+  canvas.width = canvas.height = 600;
+
+  ctx.fillStyle = BG_COLOR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  document.addEventListener("keydown", keydown);
+
+  function keydown(event) {
+    socket.emit("keyDown", event.keyCode);
+  }
+  gameActive = true;
+}
+
+function paintGame(state) {
+  ctx.fillStyle = BG_COLOR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const food = state.food;
+  const gridSize = state.gridsize;
+  const size = canvas.width / gridSize;
+
+  ctx.fillStyle = FOOD_COLOR;
+  ctx.fillRect(food.x * size, food.y * size, size, size);
+
+  paintPlayer(state.players[0], size, SNAKE_COLOR);
+  paintPlayer(state.players[1], size, "red");
+
+  function paintPlayer(playerState, size, color) {
+    const snake = playerState.snake;
+    ctx.fillStyle = color;
+    snake.forEach((cell) => {
+      ctx.fillRect(cell.x * size, cell.y * size, size, size);
+    });
+  }
+}
+
+function handleInit(number) {
+  playerNumber = number;
+}
+
+function handleGameState(gameState) {
+  if (!gameActive) {
+    return;
+  }
+  gameState = JSON.parse(gameState);
+  requestAnimationFrame(() => paintGame(gameState));
+}
+
+function handleGameOver(data) {
+  if (!gameActive) {
+    return;
+  }
+  data = JSON.parse(data);
+  if (data.winner === playerNumber) {
+    alert("You Win!");
+  } else {
+    alert("You Lose!");
+  }
+  gameActive = false;
+}
+
+function handleGameCode(code) {
+  gameCodeDisplay.innerText = code;
+}
+
+function handleUnknownGame() {
+  reset();
+  alert("unknown game code");
+}
+
+function handleTooManyPlayers() {
+  reset();
+  alert("too many players");
+}
+
+function reset() {
+  playerNumber = null;
+  gameCodeInput.value = "";
+  gameCodeDisplay.innerText = "";
+  initialScreen.style.display = "block";
+  gameScreen.style.display = "none";
+}
